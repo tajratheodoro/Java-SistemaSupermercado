a2b541da454a7aa29fdb412fff661e0e9164b498
Theodoro Tajra <102489217+tajratheodoro@users.noreply.github.com>
2023-05-26 17:04:12 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,20 +1,14 @@
 public class Cliente {
     private int quantClientes;
-    private int tempoAtendimento;
     private int numItens;
 
-    public Cliente(int quantClientes, int tempoAtendimento, int numItens) {
+    public Cliente(int quantClientes,int numItens) {
         this.numItens = numItens;
         this.quantClientes = quantClientes;
-        this.tempoAtendimento = tempoAtendimento;
     }
 
     public int getQuantClientes() {
         return quantClientes;
-    }
-
-    public int getTempoAtendimento() {
-        return tempoAtendimento;
     }
 
     public int getNumItens() {
